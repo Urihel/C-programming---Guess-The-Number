d9ea3e4e83c915586e40d82ca629a41fb0ec0c86
Urihel Coleman <45701213+Urihel@users.noreply.github.com>
2020-05-06 14:21:15 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h> //to generate random number seed
 
 int main()
 {
-   int n = 92;
+
+   int askUser;
    _Bool boolValue = 1;
 
+   //random  number generator
+   srand((unsigned) time(0));
+   int n;
+   n = rand()%101;
+   printf("%i\n",n);// The number you are trying to guess printed out.
+
+    // a while loop to continue to ask the user until he guesses correctly
    while(boolValue) {
 
        printf("Please Enter a Number between 1-100: ");
-       scanf("%i",&n);
+       scanf("%i",&askUser);
 
-       if((n > 0) && (n < 92)) {
+       if((askUser > 0) && (askUser < n)) {
         printf("Your answer is too Low\n");
-       }else if((n <= 100) && (n > 92)) {
+       }else if((askUser <= 100) && (askUser > n)) {
         printf("Your answer is too High\n");
-       }else if( n == 92) {
+       }else if(askUser == n) {
         printf("You guessed the number\n");
-       }else if((n <= 0) || (n > 100)) {
+        break; // breaks out of the loop once the answer is correct. 
+       }else if((askUser <= 0) || (askUser > 100)) {
         printf("You an entered a incorrect value\n");
        }
 
